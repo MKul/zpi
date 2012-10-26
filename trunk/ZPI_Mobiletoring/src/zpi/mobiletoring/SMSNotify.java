bc02825e6f78d725b8a80c43485912560b0ac987
pekacz.piotr@gmail.com <pekacz.piotr@gmail.com@7b5923b4-28a1-dae7-2d73-01f13c3f6a66>
2012-10-26 12:19:11 +0000

    Capture camera no. Notification intent contains the number.

--- trunk/ZPI_Mobiletoring/src/zpi/mobiletoring/SMSNotify.java
+++ trunk/ZPI_Mobiletoring/src/zpi/mobiletoring/SMSNotify.java
@@ -16,15 +16,17 @@ public class SMSNotify extends BroadcastReceiver
 	private static final String LOG_TAG = "SMSReceiver";
 	public static final int NOTIFICATION_ID_RECEIVED = 0x1221;
 	static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
+	static final String FROM = "654754567";
+	static final int icon = R.drawable.ic_launcher;
+	private boolean notify = false;
+	private int camNo;
 	
 	@Override
 	public void onReceive(Context context, Intent intent) 
-	{	NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+	{	
+		NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		if(intent.getAction().equals(SMSNotify.ACTION))
 		{
-			StringBuilder sb = new StringBuilder();
-			String from = new String();
-			String body = new String();
 			Bundle bundle = intent.getExtras();
 			if(bundle != null)
 			{
@@ -32,24 +34,29 @@ public class SMSNotify extends BroadcastReceiver
 				for(Object pdu : pdus)
 				{
 					SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdu);
-					from = messages.getDisplayOriginatingAddress();
-					body = messages.getDisplayMessageBody();
-					sb.append("Otrzymano SMS\nOd: ");
-					sb.append(messages.getDisplayOriginatingAddress());
-					sb.append("\n-----Wiadomoœæ-----\n");
-					sb.append(messages.getDisplayMessageBody());
+					if(messages.getDisplayOriginatingAddress().equals(SMSNotify.FROM))
+					{
+						notify = true;
+						String messageBody = messages.getDisplayMessageBody();
+						int camNoPosition = messageBody.indexOf("CAM") + 3;
+						camNo = Integer.parseInt(messageBody.substring(camNoPosition, camNoPosition+2));
+					}
 				}
 			}
-			int icon = R.drawable.ic_launcher;
-			CharSequence tickerText = "Nowa wiadomoœæ od " + from + ": " + body;
-			long when = System.currentTimeMillis();
-			Notification notification = new Notification(icon, tickerText, when);
-			CharSequence contentTitle = "Nowa wiadomoœæ SMS";
-			CharSequence contentText = sb.toString();
-			Intent notificationIntent = new Intent(context, TabHostActivity.class);
-			PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-			notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-			mNotificationManager.notify(NOTIFICATION_ID_RECEIVED, notification);
+			if(notify)
+			{
+				CharSequence tickerText = "ALARM AKTYWOWANY";
+				long when = System.currentTimeMillis();
+				Notification notification = new Notification(icon, tickerText, when);
+				CharSequence contentTitle = "System alarmowy";
+				CharSequence contentText = "Kliknij, aby w³¹czyæ podgl¹d";
+				Intent notificationIntent = new Intent(context, TabHostActivity.class);
+				notificationIntent.putExtra("camNo", camNo);
+				PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+				notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+				mNotificationManager.notify(NOTIFICATION_ID_RECEIVED, notification);
+				notify = false;
+			}
 		}
 	}
 }
