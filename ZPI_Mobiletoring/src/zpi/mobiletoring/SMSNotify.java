12df59d8546fe9a928d9419df71fadd40e829fbf
pekacz.piotr@gmail.com <pekacz.piotr@gmail.com@7b5923b4-28a1-dae7-2d73-01f13c3f6a66>
2012-10-26 10:01:52 +0000

    SMS Notification works out of the application, intent added.

--- ZPI_Mobiletoring/src/zpi/mobiletoring/SMSNotify.java
+++ ZPI_Mobiletoring/src/zpi/mobiletoring/SMSNotify.java
@@ -1,5 +1,8 @@
 package zpi.mobiletoring;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -16,10 +19,12 @@ public class SMSNotify extends BroadcastReceiver
 	
 	@Override
 	public void onReceive(Context context, Intent intent) 
-	{	
+	{	NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 		if(intent.getAction().equals(SMSNotify.ACTION))
 		{
 			StringBuilder sb = new StringBuilder();
+			String from = new String();
+			String body = new String();
 			Bundle bundle = intent.getExtras();
 			if(bundle != null)
 			{
@@ -27,14 +32,24 @@ public class SMSNotify extends BroadcastReceiver
 				for(Object pdu : pdus)
 				{
 					SmsMessage messages = SmsMessage.createFromPdu((byte[]) pdu);
+					from = messages.getDisplayOriginatingAddress();
+					body = messages.getDisplayMessageBody();
 					sb.append("Otrzymano SMS\nOd: ");
 					sb.append(messages.getDisplayOriginatingAddress());
 					sb.append("\n-----Wiadomoœæ-----\n");
 					sb.append(messages.getDisplayMessageBody());
 				}
 			}
-			Log.i(SMSNotify.LOG_TAG, "[SMSApp] onReceiveIntent: " + sb);
-			Toast.makeText(context, sb.toString(), Toast.LENGTH_LONG).show();
+			int icon = R.drawable.ic_launcher;
+			CharSequence tickerText = "Nowa wiadomoœæ od " + from + ": " + body;
+			long when = System.currentTimeMillis();
+			Notification notification = new Notification(icon, tickerText, when);
+			CharSequence contentTitle = "Nowa wiadomoœæ SMS";
+			CharSequence contentText = sb.toString();
+			Intent notificationIntent = new Intent(context, ConfigurationActivity.class);
+			PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+			notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+			mNotificationManager.notify(NOTIFICATION_ID_RECEIVED, notification);
 		}
 	}
 }
