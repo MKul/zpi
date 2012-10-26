e3666b61f18eb78608d3035ad2adce2077588384
mkoral7@gmail.com <mkoral7@gmail.com@7b5923b4-28a1-dae7-2d73-01f13c3f6a66>
2012-10-26 13:01:00 +0000

    Singleton, przechowujący adresy strumieni.

---
+++ trunk/ZPI_Mobiletoring/src/zpi/mobiletoring/ConfHolder.java
@@ -0,0 +1,30 @@
+package zpi.mobiletoring;
+
+public final class ConfHolder {
+    private final static ConfHolder ourInstance = new ConfHolder();
+    private String camera1="";
+    private String camera2="";
+    public static ConfHolder getInstance() {
+        return ourInstance;
+    }
+ 
+    //¿eby unikn¹æ automatycznego tworzenia domyœlnego, publicznego, bezargumentowego konstruktora
+    private ConfHolder() {
+    }
+
+	public String getCamera1() {
+		return camera1;
+	}
+
+	public void setCamera1(String camera1) {
+		this.camera1 = camera1;
+	}
+
+	public String getCamera2() {
+		return camera2;
+	}
+
+	public void setCamera2(String camera2) {
+		this.camera2 = camera2;
+	}
+}
